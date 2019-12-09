1ab0070910270366298e941168e5e7823715b9e9
prinjal124 <prinjalboruah@gmail.com>
2019-12-09 11:29:41 +0530

    style.css changed

--- EducationInfo/settings.py
+++ EducationInfo/settings.py
@@ -94,3 +94,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
